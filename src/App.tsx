c77af8c317257eb1b8e35b30d66d858828f07af0
Kanjana [OIL] <kanjana.pe@easysunday.com>
2023-06-25 10:35:35 +0700

    finish without css

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import { MenuService } from "./menu-service";
+import { Menu, menuService, } from "./menu-service";
 
 function App() {
-  const [state, setState] = useState<string[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const [menu, setMenu] = useState<Menu>({
+    menuName: "Strawberry Curry Omakase",
+    creativeName: "Tasteful Treat of Oriental Delight",
+    ingredients: [
+      "strawberries",
+      "lemons",
+      "curry powder",
+      "vegetable oil",
+      "salt"
+    ],
+    description: "This dish is the perfect blend of sweet and savory. Start your experience with the sweet burst of fresh strawberries, cutting through it all with the zing of lemon and the rich and spicy flavors of curry powder",
+    imageUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-eOBx37hJO3dTQO4DuaBja5ko/user-AyQ701q0tmEwbgMovCsflyA3/img-57VJfjC3NZ9pOnTokVJ0Sq87.png?st=2023-06-24T12%3A18%3A26Z&se=2023-06-24T14%3A18%3A26Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-24T01%3A53%3A31Z&ske=2023-06-25T01%3A53%3A31Z&sks=b&skv=2021-08-06&sig=dwkSIW%2BjY4Sa2AQM%2B6Vz0DosM2fHpHx2XyiHsXBQbZA%3D"
+  });
 
-  const randomMenus = () => {
-    const menuService = new MenuService();
 
-    const menus = [];
-    for (let i = 0; i < 10; i++) {
-      const menu = menuService.generateMenuAlg1();
-      menus.push(menu);
+  const createRandomMenus = async () => {
+    setLoading(true);
+
+    try {
+      const ingredients = menuService.generateIngredients();
+      const menu = await menuService.createMenuFromIngredients(ingredients);
+      setMenu(menu);
+    } finally {
+      setLoading(false);
     }
-    setState(menus);
   };
-
-  useEffect(() => {
-    randomMenus();
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Stupid restaurant</h1>
-        <ul>
-          {state.map((menu) => (
-            <li>{menu}</li>
-          ))}
-        </ul>
+        <h1>Stupid Restaurant</h1>
+        {isLoading ? <p>Wait a second, we are cooking ...</p> :
+          <>
+            <p>Menu name: {menu.creativeName}</p>
+            <p>Ingredients: {menu.ingredients.join(', ')}</p>
+            <p>description: {menu.description}</p>
+            {
+              menu.imageUrl && <img src={menu.imageUrl} alt="stupidImage" />
+            }
+          </>
+        }
 
         <button
-        onClick={randomMenus}
+          onClick={createRandomMenus}
           style={{
             height: 48,
             paddingLeft: 24,
--- src/menu-service.ts
+++ src/menu-service.ts
@@ -1,3 +1,21 @@
+import { Configuration, OpenAIApi } from "openai";
+
+interface GPTMenuResponse {
+  menuName: string;
+  creativeName: string;
+  ingredients: string[];
+  description: string;
+  dallePrompt: string;
+}
+
+export interface Menu {
+  menuName: string;
+  creativeName: string;
+  ingredients: string[];
+  description: string;
+  imageUrl?: string;
+}
+
 export class MenuService {
   base1Ingredients = [
     "boat noodle",
@@ -87,7 +105,9 @@ export class MenuService {
     "Evian Mineral Water",
   ];
 
-  generateMenuAlg1(): string {
+  constructor(private openAIApi: OpenAIApi) { }
+
+  generateIngredients(): string[] {
     const base1Index = Math.floor(Math.random() * this.base1Ingredients.length);
     const base2Index = Math.floor(Math.random() * this.base2Ingredients.length);
     const memeableIndex = Math.floor(
@@ -98,26 +118,57 @@ export class MenuService {
     const base2 = this.base2Ingredients[base2Index];
     const memeable = this.memeableIngredients[memeableIndex];
 
-    return `${base1}, ${base2}, ${memeable}`;
+    return [base1, base2, memeable];
   }
 
-  generateMenuAlg2(): string {
-    const baseIndex = Math.floor(
-      Math.random() *
-        (this.base1Ingredients.length + this.base2Ingredients.length)
-    );
-    const memeableIndex = Math.floor(
-      Math.random() * this.memeableIngredients.length
-    );
+  async createMenuFromIngredients(requiredIngredients: string[]): Promise<Menu> {
+    const gptMenuResponse = await this.generateMenuInformation(requiredIngredients);
 
-    let base: string;
-    if (baseIndex < this.base1Ingredients.length) {
-      base = this.base1Ingredients[baseIndex];
-    } else {
-      base = this.base2Ingredients[baseIndex - this.base1Ingredients.length];
+    return {
+      menuName: gptMenuResponse.menuName,
+      creativeName: gptMenuResponse.creativeName,
+      ingredients: gptMenuResponse.ingredients,
+      description: gptMenuResponse.description,
+      imageUrl: await this.createImageFromMenu(gptMenuResponse.dallePrompt)
     }
-    const memeable = this.memeableIngredients[memeableIndex];
+  }
 
-    return `${base}, ${memeable}`;
+  private async generateMenuInformation(requiredIngredients: string[]): Promise<GPTMenuResponse> {
+    const prompt = `create a menu with ${requiredIngredients.join(', ')} provided information in this json format, please provided the creative name of the menu that people would amaze it, split each ingredients in to arrays element, please specify the DALL-E prompt for generating this menu image which looks disturbing, disgusting and horrible as much as possible
+    {
+      "menuName": "string",
+      "creativeName": "string",
+      "ingredients": "string",
+      "description": "string",
+      "dallePrompt": "string",
+    }
+    
+    return me only 1 menu`
+
+    const response = await this.openAIApi.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      max_tokens: 500,
+      temperature: 1,
+    });
+    const responseText = response.data.choices[0].text ?? '{}'
+    console.log('oildel', responseText);
+
+    return JSON.parse(responseText.substring(responseText.indexOf('{'), responseText.indexOf('}') + 1));
+  }
+
+  private async createImageFromMenu(dallePrompt: string): Promise<string | undefined> {
+    const response = await this.openAIApi.createImage({
+      prompt: dallePrompt,
+      n: 1,
+      size: "512x512",
+    });
+    return response.data.data[0].url;
   }
 }
+
+export const menuService = new MenuService(
+  new OpenAIApi(new Configuration({
+    apiKey: '',
+  }))
+);
