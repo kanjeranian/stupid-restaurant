b86ce5c1ff76198c874d37856f0bf53d6de9ecec
Kanjana [OIL] <kanjana.pe@easysunday.com>
2023-06-25 14:41:06 +0700

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Menu, createRandomMenu, getLatestMenu } from "./menu-service";
+import header from './assets/header.png';
+import logo from './assets/logo.png';
+import tape from './assets/tape.png';
+import ordernow from './assets/ordernow.png';
+import orderAgain from './assets/orderAgain.png';
+import loading from './assets/loading.gif';
+import cookingText from './assets/cooking-text.gif';
 
 function App() {
   const [isLoading, setLoading] = useState(false);
@@ -17,40 +24,41 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    getLatestMenu().then(menu => setMenu(menu));
-  }, [])
+  // useEffect(() => {
+  //   getLatestMenu().then(menu => setMenu(menu));
+  // }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Ramsey's Aveji Kitchen</h1>
-        {isLoading ? (
-          <p>Wait a second, we are cooking ...</p>
-        ) : (
-          menu && (
-            <>
-              <p>Menu name: {menu.creativeName}</p>
-              <p>Ingredients: {menu.ingredients.join(", ")}</p>
-              <p>description: {menu.description}</p>
-              {menu.imageUrl && <img src={menu.imageUrl} alt="stupidImage" />}
-            </>
-          )
-        )}
+      <div className="container">
+        <img src={header} alt="" className="headerImg" />
+        {!isLoading && !menu && <img src={logo} alt="" className="logoImg" />}
+        {!isLoading && !menu && <img src={ordernow} alt="" className="orderButton" onClick={onCreateNewMenu} />}
+        
+        {isLoading && <div className="loading-container">
+          <img src={loading} alt="" className="loading" />
+          <img src={cookingText} alt="" className="cookingText" />
+        </div>}
 
-        <button
-          onClick={onCreateNewMenu}
-          style={{
-            height: 48,
-            paddingLeft: 24,
-            paddingRight: 24,
-            borderRadius: 24,
-            fontSize: 18,
-          }}
-        >
-          Generate
-        </button>
-      </header>
+        {!isLoading && menu && <div className="board-container">
+          <div className="board-content">
+            <div className="menuName">{menu.creativeName}</div>
+            <div className="menuIngredients"> {menu.ingredients.join(" | ")}</div>
+            <div className="menuImageContainer">
+              <img src={tape} alt="" className="tape" />
+              {menu.imageUrl && <img src={menu.imageUrl} alt="menuImage" className="menuImage" />}
+            </div>
+            <div className="description"> Description </div>
+            <div className="menuDescription">{menu.description}</div>
+
+            <img src={orderAgain} alt="" className="orderButton" onClick={onCreateNewMenu} />
+
+          </div>
+        </div>}
+
+        
+
+      </div>
     </div>
   );
 }
